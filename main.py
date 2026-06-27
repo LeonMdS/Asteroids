8e437dc9cf487405261cb06dd42e23414d2e81a2
LeonMdS <leonmds1@gmail.com>
2026-06-26 21:58:39 -0300

    added collision death

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
 
 def main():
+    # Test prints
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    # Basic pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    # Creating groups for the game objects
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
+    # Setting default containers for the game objects
     AsteroidField.containers = updatable
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, drawable, updatable)
+
+    # Creating the game objects
     asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
+        # Logging the state
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+
+        # Updating the game objects
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        # Drawing the game objects
+        screen.fill("black")
         for to_draw in drawable:
             to_draw.draw(screen)
+
+        # Display flip
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # Limit to 60 FPS and get delta time in seconds
 
